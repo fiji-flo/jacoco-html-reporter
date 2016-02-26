ed02fb63b597224f6fc1614403b111b6d8356e4c
Florian Merz <florian.merz@goeuro.com>
2016-02-26 11:08:30 +0100

    fix multi project not displaying source

--- src/main/java/com/goeuro/coverage/CoverageReporter.java
+++ src/main/java/com/goeuro/coverage/CoverageReporter.java
@@ -4,19 +4,42 @@ import org.jacoco.core.analysis.Analyzer;
 import org.jacoco.core.analysis.CoverageBuilder;
 import org.jacoco.core.analysis.IBundleCoverage;
 import org.jacoco.core.tools.ExecFileLoader;
-import org.jacoco.report.DirectorySourceFileLocator;
 import org.jacoco.report.FileMultiReportOutput;
 import org.jacoco.report.IReportVisitor;
+import org.jacoco.report.InputStreamSourceFileLocator;
 import org.jacoco.report.html.HTMLFormatter;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class CoverageReporter {
-    private static void create(String outDir, String[] execFiles, String[] classDirs, String[] srcDirs)  throws IOException {
-        HTMLFormatter htmlFormatter = new HTMLFormatter();
-        IReportVisitor visitor = htmlFormatter
-                .createVisitor(new FileMultiReportOutput(new File(outDir, "report")));
+    public static class DirectoriesSourceFileLocator extends InputStreamSourceFileLocator {
+
+        private final String[] directories;
+
+        public DirectoriesSourceFileLocator(final String[] directories,
+        final String encoding, final int tabWidth) {
+            super(encoding, tabWidth);
+            this.directories = directories;
+        }
+
+        @Override
+        protected InputStream getSourceStream(final String path) throws IOException {
+            for (String directory: directories) {
+                final File file = new File(directory, path);
+                if (file.exists()) {
+                    return new FileInputStream(file);
+                }
+            }
+            return null;
+        }
+    }
+
+    private static void create(String outDir, String[] execFiles, String[] classDirs, String[] srcDirs, HTMLFormatter formatter)  throws IOException {
+        IReportVisitor visitor = formatter
+        .createVisitor(new FileMultiReportOutput(new File(outDir, "report")));
         CoverageBuilder coverageBuilder = new CoverageBuilder();
         ExecFileLoader execFileLoader = new ExecFileLoader();
         for (String execFile: execFiles) {
@@ -24,19 +47,16 @@ public class CoverageReporter {
         }
         Analyzer analyzer = new Analyzer(execFileLoader.getExecutionDataStore(), coverageBuilder);
 
-       for (String classDir: classDirs) {
-           analyzer.analyzeAll(new File(classDir));
-       }
+        for (String classDir: classDirs) {
+            analyzer.analyzeAll(new File(classDir));
+        }
 
-       visitor.visitInfo(execFileLoader.getSessionInfoStore().getInfos(),
-               execFileLoader.getExecutionDataStore().getContents());
+        visitor.visitInfo(execFileLoader.getSessionInfoStore().getInfos(),
+        execFileLoader.getExecutionDataStore().getContents());
 
 
         IBundleCoverage bundle = coverageBuilder.getBundle("report");
-        for (String srcDir: srcDirs) {
-            visitor.visitBundle(bundle, new DirectorySourceFileLocator(
-                    new File(srcDir), "utf-8", 4));
-        }
+        visitor.visitBundle(bundle, new DirectoriesSourceFileLocator(srcDirs, "utf-8", 4));
 
         visitor.visitEnd();
     }
@@ -48,10 +68,10 @@ public class CoverageReporter {
         }
         try {
             create(
-                    args[0],
-                    args[1].split(";"),
-                    args[2].split(";"),
-                    args[3].split(";")
+            args[0],
+            args[1].split(";"),
+            args[2].split(";"),
+            args[3].split(";"), new HTMLFormatter()
             );
         } catch (IOException e) {
             System.out.print("Unable to generate report: ");
